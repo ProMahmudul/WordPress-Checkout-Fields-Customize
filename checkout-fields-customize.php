ac719a24042e67699890f2032d48085d7a6f2391
promahmudul <mahmudul.coder@gmail.com>
2020-07-20 15:54:31 +0600

    address_1 not working fix

--- checkout-fields-customize.php
+++ checkout-fields-customize.php
@@ -13,7 +13,7 @@ function custom_override_checkout_fields( $fields ) {
     //unset($fields['billing']['billing_country']);
     //unset($fields['billing']['billing_state']);
     //unset($fields['billing']['billing_phone']);
-    unset($fields['order']['order_comments']);
+    //unset($fields['order']['order_comments']);
     unset($fields['billing']['billing_address_2']);
     unset($fields['billing']['billing_postcode']);
     unset($fields['billing']['billing_company']);
@@ -33,7 +33,16 @@ function custom_required_override_checkout_fields( $fields ) {
 add_filter( 'woocommerce_checkout_fields' , 'custom_rename_override_checkout_fields' );
 
 function custom_rename_override_checkout_fields( $fields ) {
-     $fields['billing']['billing_first_name']['label'] = 'Full Name';
-     //$fields['billing']['billing_address_1']['label'] = 'Address';
-     return $fields;
+    $fields['billing']['billing_first_name']['label'] = 'Full Name';
+	$fields['billing']['billing_first_name']['placeholder'] = 'Your Full Name';
+	$fields['billing']['billing_email']['placeholder'] = 'Your Email';
+	$fields['billing']['billing_phone']['placeholder'] = 'Your Phone Number';
+    return $fields;
+}
+
+add_filter( 'woocommerce_default_address_fields' , 'override_default_address_fields' );
+function override_default_address_fields( $address_fields ) {
+	$address_fields['address_1']['label'] = 'Address';
+	$address_fields['address_1']['placeholder'] = 'Your Address';
+	return $address_fields;
 }
